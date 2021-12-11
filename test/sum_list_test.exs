c761fb852072f9cd9dfdc428266053e72bd90995
Joao Gomes <joaopfg.2002@gmail.com>
2021-12-11 01:59:59 +0000

    Finish Module

--- test/sum_list_test.exs
+++ test/sum_list_test.exs
@@ -1,3 +1,17 @@
 defmodule SumListTest do
   use ExUnit.Case
+
+  describe "call/1" do
+
+    test "i should be able to return list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+
+      expected_response = 6
+
+      assert response == expected_response
+    end
+
+  end
 end
